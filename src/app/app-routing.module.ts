042ade3c2df3590d17ee87d4aa90e2b1936bdf84
Hari Nair <3079@wingsfinancial.com>
2024-04-23 15:44:51 -0500

    button color, rediret to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from "./components/login/login.component";
 import { HomeComponent } from "./components/home/home.component";
 
 const routes: Routes = [
-  //{ path: "", pathMatch: "full", redirectTo: "home" },
+  { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent },
   { path: "login", component: LoginComponent },
 ];

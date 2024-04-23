2b1f8668b97b2759a334bd8b18b31e3b18913b8a
Hari Nair <3079@wingsfinancial.com>
2024-04-23 12:55:18 -0500

    Router modified, reformatted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app-root/app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { LeftnavComponent } from './components/leftnav/leftnav.component';
-import { RightnavComponent } from './components/rightnav/rightnav.component';
-import { MainBodyComponent } from './components/main-body/main-body.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.root/app.root.component";
+import { FooterComponent } from "./components/footer/footer.component";
+import { LeftnavComponent } from "./components/leftnav/leftnav.component";
+import { RightnavComponent } from "./components/rightnav/rightnav.component";
+import { MainBodyComponent } from "./components/main-body/main-body.component";
 
-import { RouterModule } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from "./components/login/login.component";
 
 @NgModule({
   declarations: [
@@ -18,19 +17,10 @@ import { LoginComponent } from './components/login/login.component';
     LeftnavComponent,
     RightnavComponent,
     MainBodyComponent,
-    LoginComponent
+    LoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot([
-        {path: '', pathMatch: 'full', redirectTo: 'home'},
-        {path: 'home', component: AppComponent},
-        {path: 'login', component: LoginComponent}
-  ])
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/app.root/app.root.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.root.component.html',
+  styleUrls: ['./app.root.component.css']
+})
+
+// export class AppComponent implements OnInit {
+//   title = 'Angular 16';
+
+//   constructor(){}
+
+//   ngOnInit(): void {
+    
+//   }
+
+
+// }
+
+export class AppComponent {
+  title = 'Angular 16';
+}
